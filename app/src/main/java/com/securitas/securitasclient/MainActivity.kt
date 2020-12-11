fba54d102b40117cf15b5e868f1600a17050c3c5
The1nS1ghT <makss.past@gmail.com>
2020-12-11 17:35:19 +0300

    main экран

--- app/src/main/java/com/securitas/securitasclient/MainActivity.kt
+++ app/src/main/java/com/securitas/securitasclient/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.securitas.securitasclient
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,4 +11,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         supportActionBar?.hide();
     }
+
+
+    fun registration(view: View){
+        //создание нового активити (окно регистрации)
+        val registration = Intent(this,RegistrationActivity::class.java)
+        //старт нового активити
+        startActivity(registration)}
+
+
+
+
 }
